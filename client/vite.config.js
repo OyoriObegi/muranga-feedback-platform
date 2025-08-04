a286bcd51d5c128e4f8bf377beb5b1ec5eb2eb81
Oyori Obegi <oyoriobegi18@gmail.com>
2025-08-04 19:51:07 +0300

    Simplify vite config and fix build script

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -13,14 +12,4 @@ export default defineConfig({
       },
     },
   },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  }
 })
